ac243be7fc4ebeeed98ef173e35cbbd913256670
Zeyu-Li <zeyu.li23@gmail.com>
2021-10-10 13:01:10 -0600

    added random + dealloc

--- main.c
+++ main.c
@@ -3,26 +3,14 @@
 */ 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
 
 struct Block {
-    short value;
+    char value;
     struct Block* next;
     struct Block* parent;
 } UwU = {0, NULL, NULL};
-
-struct Block* next(struct Block* curr) {
-    if (curr->next != NULL) {
-        return curr->next;
-    } else {
-        curr->next = malloc(sizeof (struct Block));
-        curr->next->value = 0;
-        // if next cannot be alloc
-        if (curr->next == NULL)
-            return NULL;
-        else 
-            return curr->next;
-    }
-}
 
 struct Block* next(struct Block* curr) {
     if (curr->next != NULL) {
@@ -43,6 +31,8 @@ struct Block* next(struct Block* curr) {
 int main(int argc, char *argv[]) {
     struct Block* head = &UwU;
     struct Block* curr = &UwU;
+    // Use current time as seed for random generator
+    srand(time(0));
 
     // TODO: convert char to UTF-8 emojis
 
@@ -57,13 +47,27 @@ int main(int argc, char *argv[]) {
         else if (currChar == '<') {
             // if parent exists return parent, otherwise return current
             curr = curr->parent ? curr->parent : curr;
+        } else if (currChar == '?') {
+            // if parent exists return parent, otherwise return current
+            curr->value = rand() % 128;
         }
     }
+
+    // dealloc from head
+    if (head->next != NULL) {
+        struct Block* deletePtr = head->next;
+        while ((deletePtr = deletePtr->next)) {
+            free(deletePtr->parent);
+        }
+        free(deletePtr);
+    }
+    // since struct is statically defined, no need to dealloc
 
     // new line
     putchar('\n');
 
+
     return 0;
 }
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++. 👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆🥺
+// >>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++. 👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆🥺
