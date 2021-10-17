53030d90942860c786719973bf01c05cdc0435af
Zeyu-Li <zeyu.li23@gmail.com>
2021-10-17 16:34:50 -0600

    can print

--- main.c
+++ main.c
@@ -6,6 +6,16 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define STACK_SIZE      512
+
+#define STACK_PUSH(A)   (STACK[SP++] = A)
+#define STACK_POP()     (STACK[--SP])
+#define STACK_EMPTY()   (SP == 0)
+#define STACK_FULL()    (SP == STACK_SIZE)
+
+static unsigned short STACK[STACK_SIZE];
+static unsigned int SP = 0;
+
 struct Block {
     char value;
     struct Block* next;
@@ -41,14 +51,30 @@ int main(int argc, char *argv[]) {
 
     int currInputChar;
     int *inputCounter = code;
+    int counter = 0;
     while ((currInputChar = getchar()) != EOF && currInputChar != '\n') {
         *inputCounter = currInputChar;
         inputCounter++;
+        counter++;
     }
 
+    char jump_table[32768] = {0};
+    int *preCounter = code;
     // hash map for pre-complied [] blocks
+    for (int i = 0; i < counter / 4 + 1; i++) {
+        if (*preCounter == 240 && *(preCounter+1) == 159 && *(preCounter+2) == 152 &&  *(preCounter+2) == 146) {
+            STACK_PUSH(i);
+        } else if (*preCounter == 240 && *(preCounter+1) == 159 && *(preCounter+2) == 152 &&  *(preCounter+2) == 161) {
+            int start = STACK_POP();
+            jump_table[start] = i;
+            jump_table[i] = start;
+        }
+        // currChar == 152 && nextChar == 161
+        preCounter+=4;
+    }
 
     int *pc = code;
+    int ptr = 0;
     int currChar, nextChar;
     int stackCount = 0;
     while (*pc != EOF && *pc != '\n' && *pc != '\0') {
@@ -68,9 +94,18 @@ int main(int argc, char *argv[]) {
         currChar = *pc;
         nextChar = *(++pc);
         // printf("%d", curr.value);
-        if (currChar == 145 && nextChar == 134) curr->value++;
-        else if (currChar == 145 && nextChar == 135) curr->value--;
-        else if (currChar == 165 && nextChar == 186) putchar(curr->value);
+        if (currChar == 145 && nextChar == 134) {
+            if (curr->value < 255) curr->value++;
+            else curr->value = 0;
+        }
+        else if (currChar == 145 && nextChar == 135) {
+            if (curr->value > 0) curr->value--;
+            else curr->value = 255;
+        }
+        else if (currChar == 165 && nextChar == 186) {
+            // putchar(curr->value);
+            printf("%d ", curr->value);
+        }
         else if (currChar == 152 && nextChar == 179) curr->value = getchar();
         else if (currChar == 145 && nextChar == 137) curr = next(curr);
         else if (currChar == 145 && nextChar == 136) {
@@ -79,44 +114,24 @@ int main(int argc, char *argv[]) {
         } else if (currChar == 165 && nextChar == 180) {
             // if parent exists return parent, otherwise return current
             curr->value = rand() % 128;
-        } else if (currChar == 152 && nextChar == 146) {
-            // if parent exists return parent, otherwise return current
-            if (curr->value == 0) {
-                // go to ']'
-                while (*pc != ']') {
-                    // stop from going over
-                    if (*pc == EOF && *pc == '\0') {
-                        pc--;
-                        break;
-                    }
-                    pc++;
-                }
-            } else {
-                stackCount++;
-            }
-        } else if (currChar == 152 && nextChar == 161) {
-            // if parent exists return parent, otherwise return current
-            if (curr->value == 0) {
-                // go to ']'
-                while (*pc != '[') {
-                    // stop from going over
-                    if (*pc == EOF && *pc == '\0') {
-                        pc--;
-                        break;
-                    }
-                    pc--;
-                }
-            } else {
-                if (stackCount <= 0) {
-                    // stack error
-                    return 1;
-                } else {
-                    stackCount--;
-                }
-            }
+        } else if (currChar == 152 && nextChar == 146 && curr->value == 0) {
+            // [
+            // get ptr to right place
+            // for (int k = ptr; k < jump_table[ptr]; k++) {
+            //     pc-=4;
+            // }
+            curr->value = jump_table[curr->value]; // get hashed value
+        } else if (currChar == 152 && nextChar == 161 && curr->value != 0) {
+            // 😡 ]
+            // get ptr to right place
+            // for (int k = ptr; k > jump_table[ptr]; k--) {
+            //     pc-=4;
+            // }
+            curr->value = jump_table[curr->value]; // get hashed value
         }
 
         pc++;
+        ptr++;
     }
 
     // dealloc from head
